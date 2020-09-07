cb64b5e94f29aa89ddc51913a4e85be4e0d7892a
Adam Cook <me@cookadam.co.uk>
2020-09-07 22:51:32 +0100

    Updated Get-MyCommands :shipit:

--- codaamok/Public/Get-MyCommands.ps1
+++ codaamok/Public/Get-MyCommands.ps1
@@ -1,10 +1,10 @@
 Function Get-MyCommands {
     [Alias("Get-MyFunctions")]
     param (
-        [String]$Profile = $PSCommandPath
+        [String]$ProfileFile = $PSCommandPath
     )
 
-    Get-Content -Path $Profile | Select-String -Pattern "^function.+" | ForEach-Object {
+    Get-Content -Path $ProfileFile | Select-String -Pattern "^function.+" | ForEach-Object {
         $result = [Regex]::Matches($_, "^function ([a-z.-]+)","IgnoreCase").Groups[1].Value
         if ($result -ne "prompt") { $result }
     } | Sort-Object

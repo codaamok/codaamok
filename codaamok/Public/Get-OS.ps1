9089322dc3a032205ec89770c035c81b44fc1d07
Adam Cook <me@cookadam.co.uk>
2020-11-04 21:28:37 +0000

    Tidy up :shipit:

--- codaamok/Public/Get-OS.ps1
+++ codaamok/Public/Get-OS.ps1
@@ -1,22 +1,28 @@
 function Get-OS {
     Param (
         [Parameter(Mandatory)]
-        [String]$ComputerName,
+        [String[]]$ComputerName,
         [Parameter()]
         [PSCredential]$Credential
     )
+
     $newCimSessionSplat = @{
         ComputerName = $ComputerName
         ErrorAction = "Stop"
     }
+
     if ($PSBoundParameters.ContainsKey("Credential")) {
         $newCimSessionSplat["Credential"] = $Credential
     }
+
     $Session = New-CimSession @newCimSessionSplat 
+    
     $getCimInstanceSplat = @{
         Query = "Select Caption from Win32_OperatingSystem"
         CimSession = $Session
     }
+
     Get-CimInstance @getCimInstanceSplat | Select-Object -ExpandProperty Caption
+    
     Remove-CimSession $Session
 }

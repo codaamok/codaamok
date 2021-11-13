0b7e4e663df9fdc4b694454233f12b0ac12d14e8
Adam Cook <me@cookadam.co.uk>
2021-11-13 23:25:21 +0000

    Removed duplicate Update-Profile function in the module, in favour of the one defined in profile.ps1

--- codaamok/Public/Update-Profile.ps1
+++
@@ -1,6 +0,0 @@
-Function Update-Profile {
-    $Module = Get-Module codaamok 
-    Copy-Item -Path "$($Module.ModuleBase)\profile.ps1" -Destination $profile.CurrentUserAllHosts -Confirm
-    '. $profile.CurrentUserAllHosts' | clip
-    Write-Host "Paste your clipboard"
-}

19a532bc0add30c6bd1108bee2cf1817fb84e1cb
Adam Cook <me@cookadam.co.uk>
2021-12-07 21:27:49 +0000

    Fixed Install-Profile

--- codaamok/Public/Install-Profile.ps1
+++ codaamok/Public/Install-Profile.ps1
@@ -1,4 +1,5 @@
 function Install-Profile {
+    $Module = Get-Module codaamok -PassThru -ErrorAction "Stop"
     Copy-Item -Path "$($Module.ModuleBase)\profile.ps1" -Destination $profile.CurrentUserAllHosts -Confirm -ErrorAction "Stop"
     '. $profile.CurrentUserAllHosts' | clip
     Write-Host "Paste your clipboard"
